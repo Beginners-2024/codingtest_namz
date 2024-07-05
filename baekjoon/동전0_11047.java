878bbb9693a6ed3af9c786905fefbbae1c52af05
namzisun <namzisun@gmail.com>
2024-07-05 23:25:44 +0900

    주석 추가

--- baekjoon/동전0_11047.java
+++ baekjoon/동전0_11047.java
@@ -18,7 +18,7 @@ public class 동전0_11047 {
 		for (int i = 0; i < N; ++i){
 			int value = Integer.parseInt(br.readLine());
 
-			if (value > K) break;
+			if (value > K) break;	// 동전의 금액이 K보다 크면 입력 중단
 			coin.add(value);
 		}
 		N = coin.size();
@@ -26,6 +26,11 @@ public class 동전0_11047 {
 		System.out.println(countMoney());
 	}
 
+	/**
+	 * 현재 동전의 금액이 남은 총 금액보다 크면, 최대한의 개수로 동전을 사용 & 총 금액 줄임
+	 *
+	 * @return 사용한 동전 개수
+	 */
 	public static int countMoney() {
 		int countOfCoin = 0;
 
--- baekjoon/회의실배정_1931.java
+++ baekjoon/회의실배정_1931.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class 회의실배정_1931 {
 
 	private static int N;
-	private static int[][] meetingTime;
+	private static int[][] meetingTime;	// 회의 시간 배열
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +20,8 @@ public class 회의실배정_1931 {
 			meetingTime[i][1] = Integer.parseInt(st.nextToken());
 		}
 
+
+		// 종료 시간 기준으로 오름차순, 중복값이 있다면 시작 시간 기준으로 오름차순
 		Arrays.sort(meetingTime, (a, b) -> {
 			if (a[1] != b[1]) return a[1] - b[1];
 			else return a[0] - b[0];
@@ -28,6 +30,11 @@ public class 회의실배정_1931 {
 		System.out.println(makeTimeTable());
 	}
 
+	/**
+	 * 시작시간이 종료시간보다 느리면 회의 진행 가능
+	 *
+	 * @return 가능한 회의 개수
+	 */
 	public static int makeTimeTable() {
 		int countOfMeeting = 0;
 
