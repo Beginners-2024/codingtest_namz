9d3309f696b8459a7864a26f2e0bfb9d2ccea8fd
namzisun <namzisun@gmail.com>
2024-07-08 17:43:05 +0900

    풀이 완료. 25분

--- baekjoon/미로탐색_2178.java
+++ baekjoon/미로탐색_2178.java
@@ -10,8 +10,15 @@ public class 미로탐색_2178 {
 
 	private static int[] dr = {-1, 1, 0, 0};
 	private static int[] dc = {0, 0, -1, 1};
+	private static boolean[][] visit;
 
-	private static int minCount = 1;
+	private static class Pair {
+		int r, c;
+		Pair(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,6 +28,8 @@ public class 미로탐색_2178 {
 		M = Integer.parseInt(st.nextToken());
 
 		maze = new int[N][M];
+		visit = new boolean[N][M];
+
 		for (int r = 0; r < N; ++r) {
 			String s = br.readLine();
 			for (int c = 0; c < M; ++c) {
@@ -28,6 +37,34 @@ public class 미로탐색_2178 {
 			}
 		}
 
+		makeRoute();
+		System.out.println(maze[N-1][M-1]);
+	}
 
+	private static void makeRoute() {
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(0, 0));
+		visit[0][0] = true;
+
+		while (!q.isEmpty()) {
+			int r = q.peek().r;
+			int c = q.peek().c;
+			q.poll();
+
+			for (int i = 0; i < 4; ++i) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+
+				if (!isValid(nr, nc) || maze[nr][nc] == 0 || visit[nr][nc]) continue;
+
+				maze[nr][nc] = maze[r][c] + 1;
+				visit[nr][nc] = true;
+				q.add(new Pair(nr, nc));
+			}
+		}
+	}
+
+	private static boolean isValid(int r, int c) {
+		return 0 <= r && r < N && 0 <= c && c < M;
 	}
 }
