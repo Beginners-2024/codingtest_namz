603bc695a3e516f873d571ef61208f02c178c19e
namzisun <namzisun@gmail.com>
2024-07-15 22:19:54 +0900

    주석

--- baekjoon/트리순회_1991.java
+++ baekjoon/트리순회_1991.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class 트리순회_1991 {
 
 	private static int N;
+
 	private static class Node {
 		Node left, right;
 		char name;
@@ -21,6 +22,7 @@ public class 트리순회_1991 {
 
 		StringTokenizer st;
 
+		// step 1 - 트리 생성
 		Tree tree = new Tree();
 		for (int i = 0; i < N; ++i) {
 			st = new StringTokenizer(br.readLine());
@@ -32,6 +34,7 @@ public class 트리순회_1991 {
 			tree.create(name, left, right);
 		}
 
+		// step 2 - 순회
 		tree.preorder(tree.root);
 		System.out.println();
 		tree.inorder(tree.root);
@@ -43,51 +46,67 @@ public class 트리순회_1991 {
 		public Node root;
 
 		public void create(char name, char left, char right) {
+			// 루트 노드 생성
 			if (name == 'A') {
 				root = new Node(name);
 				root.left = new Node(left);
 				root.right = new Node(right);
-			} else {
+			}
+			// 루트 노드가 아니라면 search에서 재귀를 통해 name을 root로 갖는 노드를 찾아라
+			else {
 				Node node = search(root, name);
 				node.left = new Node(left);
 				node.right = new Node(right);
 			}
 		}
 
+		// 재귀
 		public Node search(Node node, char name) {
+			// 재귀 종결조건
+			// 노드가 끝났거나(서치 실패), 노드를 찾았거나(서치 성공)
 			if (node == null) return null;
 			if (node.name == name) return node;
 
+			// 왼쪽부터 탐색하고
 			Node leftNode = search(node.left, name);
 			if (leftNode != null) return leftNode;
 
+			// 왼쪽에서 못찾았으면 오른쪽 탐색
 			return search(node.right, name);
 		}
+
+		/**
+		 * < 재귀 종결 조건 >
+		 * 자식 노드가 없는 경우 끝
+		 *
+		 * < 순회 방식 >
+		 * 현재 노드를 (루트)라고 생각하고 출력 시점을 정하면 됨.
+		 * "루트 == 출력" 이라고 생각!
+		 */
 
 		public void preorder(Node node) {
 			if (node.name == '.') return ;
 
-			System.out.print(node.name);
-
-			preorder(node.left);
-			preorder(node.right);
+			System.out.print(node.name);	// 1. 출력 (루트)
+			preorder(node.left);			// 2. 왼쪽
+			preorder(node.right);			// 3. 오른쪽
 		}
 
 		public void inorder(Node node) {
 			if (node.name == '.') return;
 
-			inorder(node.left);
-			System.out.print(node.name);
-			inorder(node.right);
+			inorder(node.left);				// 1. 왼쪽
+			System.out.print(node.name);	// 2. 출력 (루트)
+			inorder(node.right);			// 3. 오른쪽
 
 		}
 
 		public void postorder(Node node) {
 			if (node.name == '.') return ;
 
-			postorder(node.left);
-			postorder(node.right);
-			System.out.print(node.name);
+			postorder(node.left);			// 1. 왼쪽
+			postorder(node.right);			// 2. 오른쪽
+			System.out.print(node.name);	// 3. 출력 (루트)
 		}
 	}
 }
