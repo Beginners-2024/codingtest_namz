95f6a9fcd35ff859dde43355edcd1a369ea8c691
namzisun <namzisun@gmail.com>
2024-07-15 19:34:49 +0900

    주석

--- baekjoon/로프_2217.java
+++ baekjoon/로프_2217.java
@@ -10,6 +10,7 @@ public class 로프_2217 {
 	private static int maxWeight = 0;
 
 	public static void main(String[] args) throws IOException {
+		// step 1 - 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
@@ -18,8 +19,14 @@ public class 로프_2217 {
 			rope.add(Integer.parseInt(br.readLine()));
 		}
 
+		// step 2 - 오름차순 정렬
 		Collections.sort(rope);
 
+		// step 3 - 최댓값 계산
+		/**
+		 * 오름차순으로 정렬된 list에서 현재 로프부터 마지막로프까지는 현재 로프가 버틸 수 있는 최대 중량을 버틸 수 있다
+		 * => (현재 로프의 최대 중량) * (현재부터 마지막 로프까지의 개수) 가 max일 때를 찾는다
+		 */
 		for (int i = 0; i < N; ++i) {
 			maxWeight = Math.max(maxWeight, rope.get(i) * (N - i));
 		}
