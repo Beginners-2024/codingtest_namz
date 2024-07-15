7adc03efc46406444c9a8e9bf0886e2804e56188
namzisun <namzisun@gmail.com>
2024-07-15 18:59:53 +0900

    완료, 10분

--- baekjoon/로프_2217.java
+++ baekjoon/로프_2217.java
@@ -4,7 +4,26 @@ import java.util.*;
 import java.io.*;
 
 public class 로프_2217 {
-	public static void main(String[] args) throws IOException {
 
+	private static int N;
+	private static List<Integer> rope = new ArrayList<>();
+	private static int maxWeight = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; ++i) {
+			rope.add(Integer.parseInt(br.readLine()));
+		}
+
+		Collections.sort(rope);
+
+		for (int i = 0; i < N; ++i) {
+			maxWeight = Math.max(maxWeight, rope.get(i) * (N - i));
+		}
+
+		System.out.println(maxWeight);
 	}
 }
