c4ec9b2c811d0e77b23787ae02748f594faf1756
namzisun <namzisun@gmail.com>
2024-07-27 15:40:31 +0900

    풀이완료 30분

--- baekjoon/회전하는_큐_1021.java
+++ baekjoon/회전하는_큐_1021.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class 회전하는_큐_1021 {
-	private static List<Integer> deque;
+	private static Deque<Integer> dq;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,23 +12,75 @@ public class 회전하는_큐_1021 {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		deque = new ArrayList<>(N);
-		for (int i = 0; i < N; ++i) {
-			deque.add(i);
+		dq = new LinkedList<>();
+
+		for (int i = 1; i < N+1; ++i) {
+			dq.addLast(i);
 		}
 
 		int answer = 0;
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; ++i) {
-			int num = Integer.parseInt(st.nextToken()) -1;
+			int num = Integer.parseInt(st.nextToken());
 			answer += findNum(num);
 		}
+
+		System.out.print(answer);
 	}
 
-	private static int findNum(int num) {
+	private static int findNum(int keyNum) {
+		int topCount = 0;
+		Deque<Integer> tmp = new LinkedList<>(dq);
+		while (true) {
+			if (tmp.peek() == keyNum) {
+				tmp.pollFirst();
+				break;
+			}
+
+			int top = tmp.pollFirst();
+			tmp.addLast(top);
+
+			topCount++;
+		}
+
+		int bottomCount = 0;
+		tmp = new LinkedList<>(dq);
+		while (true) {
+			if (tmp.peek() == keyNum) {
+				tmp.pollFirst();
+				break;
+			}
+
+			int bottom = tmp.pollLast();
+			tmp.addFirst(bottom);
+
+			bottomCount++;
+		}
+
 		int count = 0;
+		if (topCount < bottomCount) {
+			while (true) {
+				if (dq.peek() == keyNum) {
+					dq.pollFirst();
+					break;
+				}
 
+				int top = dq.pollFirst();
+				dq.addLast(top);
+			}
+			count = topCount;
+		} else {
+			while (true) {
+				if (dq.peek() == keyNum) {
+					dq.pollFirst();
+					break;
+				}
 
+				int bottom = dq.pollLast();
+				dq.addFirst(bottom);
+			}
+			count = bottomCount;
+		}
 		return count;
 	}
 }
