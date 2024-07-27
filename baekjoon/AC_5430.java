c5537c8e82f6c8443186e929f388f73994986b9b
namzisun <namzisun@gmail.com>
2024-07-27 19:15:49 +0900

    풀이완료

--- baekjoon/AC_5430.java
+++ baekjoon/AC_5430.java
@@ -3,8 +3,13 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 소요시간 : 1시간 10분
+ * 메모리 : 93216 KB
+ * 시간 : 2664 ms (list.remove(index) 사용새) / 864 ms (start, end 사용시)
+ */
 public class AC_5430 {
-
+    private static List<Integer> answerList;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,7 +19,7 @@ public class AC_5430 {
             String p = br.readLine();
             int n = Integer.parseInt(br.readLine());
 
-            List<Integer> numList = new ArrayList<>(n);
+            List<Integer> numList = new ArrayList<>();
             String str = br.readLine();
             String[] numStr = str.substring(1, str.length()-1).split(",");
 
@@ -22,22 +27,53 @@ public class AC_5430 {
                 numList.add(Integer.parseInt(numStr[j]));
             }
 
-            command(p, numList);
-
-            if (!numList.isEmpty()) System.out.println(numList);
+            StringBuilder sb = new StringBuilder();
+            if (command(p, numList)) {
+                sb.append("[");
+                for (int l = 0; l < answerList.size(); ++l) {
+                    sb.append(answerList.get(l));
+                    if (l < answerList.size() - 1)
+                        sb.append(",");
+                }
+                sb.append("]\n");
+            }
+            else sb.append("error\n");
+            System.out.print(sb.toString());
         }
     }
 
-    private static void command(String p, List<Integer> numList) {
+    private static boolean command(String p, List<Integer> numList) {
+        int countOfR = 0;
+        int start = 0, end = numList.size();
+        int count = numList.size();
+
         for (int i = 0; i < p.length(); ++i) {
             if (p.charAt(i) == 'R') {
-                Collections.reverse(numList);
+                countOfR++;
+//                < 시간초과 > Collections.reverse(list) : O(N)
+//                Collections.reverse(numList);
+
             } else if (p.charAt(i) == 'D') {
-                if (numList.isEmpty())
-                    System.out.println("error");
-                else
-                    numList.remove(0);
+                if (count == 0) return false;
+
+                if (countOfR % 2 == 1) end--;
+                else start++;
+
+                count--;
             }
         }
+
+        answerList = new ArrayList<>();
+        if (countOfR % 2 == 1) {
+            for (int i = end-1; i >= start; --i) {
+                answerList.add(numList.get(i));
+            }
+        } else {
+            for (int i = start; i < end; ++i) {
+                answerList.add(numList.get(i));
+            }
+        }
+
+        return true;
     }
 }
