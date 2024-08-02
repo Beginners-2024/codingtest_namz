d8ea96b8cc0023e2302ed0c5527f8af6fe0c4ea9
namzisun <namzisun@gmail.com>
2024-08-02 16:19:38 +0900

    풀이완료

--- baekjoon/구슬찾기_2617.java
+++ baekjoon/구슬찾기_2617.java
@@ -3,10 +3,22 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 풀이시간 : 1시간 반
+ * 메모리 : 15960 KB
+ * 시간 : 216 ms
+ * 시간복잡도 : O(N * (N + E))
+ *
+ * 1. hashMap & List 써서 트리로 구현 => 메모리 초과
+ * 		: Integer 객체의 크기와 hashMap 자체적인 공간 할당 구조 때문인듯 함
+ * 2. boolean[] 써서 가벼운 구슬, 무거운 구슬 찾고난 후 개수 세기 => 시간초과
+ * 		: true인 구슬을 찾기 위해 계속 loop 돌아야함
+ * 3. boolean[] 사용하되, check 함수에서 가벼운/무거운 구슬 찾음과 동시에 개수 세기, visit 사용 => 정답
+ */
 public class 구슬찾기_2617 {
 	private static int N, M;
 	private static Tree[] marble;
-	private static boolean[] lightArr, heavyArr;
+	private static boolean[] lightVisit, heavyVisit;
 
 	private static class Tree {
 		boolean[] light, heavy;
@@ -16,17 +28,11 @@ public class 구슬찾기_2617 {
 			light = new boolean[n];
 			heavy = new boolean[n];
 		}
-
-		void setCount() {
-			for (int i = 0; i < N; ++i) {
-				if (light[i]) lightCount++;
-				if (heavy[i]) heavyCount++;
-			}
-		}
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		// step 1 - 입력
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -46,35 +52,81 @@ public class 구슬찾기_2617 {
 
 		}
 
+		// step 2 - i번째 구슬보다 더 가벼운 / 무거운 구슬의 개수를 센다
 		for (int i = 0; i < N; ++i) {
-			lightArr = new boolean[N];
-			heavyArr = new boolean[N];
+			lightVisit = new boolean[N];
+			heavyVisit = new boolean[N];
 
-			checkLight(i, i);
-			checkHeavy(i, i);
-
-			marble[i].setCount();
+			checkLight(i);
+			checkHeavy(i);
 		}
 
+		// step 3 - mid가 될 수 없는 구슬을 찾는다
 		System.out.println(findNoMiddle());
 	}
 
-	private static void checkLight(int root, int now) {
-		for (int next =  0; next < N; ++next) {
-			if (marble[now].light[next] == false) continue;
-			marble[root].light[next] = true;
-			checkLight(root, next);
+	/**
+	 * step 2 (1) 가벼운 구슬을 찾는 함수 (bfs)
+	 *
+	 * @param root i 번째 구슬 (트리에서 부모 노드 역할)
+	 */
+	private static void checkLight(int root) {
+		Queue<Integer> q = new LinkedList<>();
+
+		q.add(root);
+		lightVisit[root] = true;
+
+		int count = -1;
+
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			count++;
+			for (int i = 0; i < N; ++i) {
+				if (!marble[now].light[i] || lightVisit[i]) continue;
+
+				lightVisit[i] = true;
+				q.add(i);
+			}
 		}
+		marble[root].lightCount = count;
 	}
 
-	private static void checkHeavy(int root, int now) {
-		for (int next =  0; next < N; ++next) {
-			if (marble[now].heavy[next] == false) continue;
-			marble[root].heavy[next] = true;
-			checkHeavy(root, next);
+	/**
+	 * step 2 (2) - 무거운 구슬을 찾는 함수 (bfs)
+	 *
+	 * @param root
+	 */
+	private static void checkHeavy(int root) {
+		Queue<Integer> q = new LinkedList<>();
+
+		q.add(root);
+		heavyVisit[root] = true;
+
+		int count = -1;
+
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			count++;
+
+			for (int i = 0; i < N; ++i) {
+				if (!marble[now].heavy[i] || heavyVisit[i]) continue;
+
+				heavyVisit[i] = true;
+				q.add(i);
+			}
 		}
+		marble[root].heavyCount = count;
 	}
 
+	/**
+	 * step 3 - 중간이 될 수 없는 구슬의 개수를 세는 함수
+	 *
+	 * 중간이 될 수 없는 조건
+	 * 1. 가벼운 구슬의 개수가 이미 중간값보다 많음
+	 * 2. 무거운 구슬의 개수가 이미 중간값보다 많음
+	 *
+	 * @return 중간이 될 수 없는 구슬의 개수
+	 */
 	private static int findNoMiddle() {
 		int mid = (N + 1) / 2;
 		int lightCount = mid - 1;
