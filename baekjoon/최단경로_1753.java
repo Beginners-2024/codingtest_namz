ed31b30f624cb73067c95d18de666c393b7fb3a5
namzisun <namzisun@gmail.com>
2024-08-12 14:51:23 +0900

    init

---
+++ baekjoon/최단경로_1753.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ */
+public class 최단경로_1753 {
+	private static int V, E;
+	private static int K;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+
+		K = Integer.parseInt(br.readLine());
+
+
+	}
+}
